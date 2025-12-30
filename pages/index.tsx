654d0fec8301d210b4d80312a09f4537a4084d7a
chiachang100 <chiachang100@gmail.com>
2025-12-29 18:23:27 -0800

    Adjust the app version, add the display of rolf-webapp.[png|svg].

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { APP_VERSION } from "../lib/version";
+import Image from "next/image";
 
 interface HomeProps {
   message: string;
@@ -24,6 +25,15 @@ const Home: React.FC<HomeProps> = ({ message }) => (
         This page was statically generated at build time using Next.js Static
         Site Generation (SSG).
       </p>
+
+      <div style={{ position: "relative", width: "100%", height: "500px" }}>
+        <Image
+          src="/images/rolf-webapp.png"
+          alt="ROLF WebApp"
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </div>
     </main>
   </>
 );
--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { APP_VERSION } from "../lib/version";
+import Image from "next/image";
 
 interface SSRProps {
   message: string;
@@ -27,6 +28,14 @@ const SSRPage: React.FC<SSRProps> = ({ message, time }) => (
         This page was dynamically generated using Next.js Server-Side Rendering
         (SSR). <b>Rendered on the server at: {time}</b>
       </p>
+      <div style={{ position: "relative", width: "100%", height: "500px" }}>
+        <Image
+          src="/images/rolf-webapp.svg"
+          alt="ROLF WebApp"
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </div>
     </main>
   </>
 );
