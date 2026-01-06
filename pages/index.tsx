673ab7569652609d7d2e0fd228a3be305c84cee2
chiachang100 <chiachang100@gmail.com>
2026-01-06 13:11:56 -0800

    Add buttons and images.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import { APP_VERSION } from "../lib/version";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface HomeProps {
   message: string;
@@ -14,28 +15,48 @@ export const getStaticProps = async () => {
   };
 };
 
-const Home: React.FC<HomeProps> = ({ message }) => (
-  <>
-    <Header />
-    <main>
-      <h1>
-        {message} v{APP_VERSION}
-      </h1>
-      <p>
-        This page was statically generated at build time using Next.js Static
-        Site Generation (SSG).
-      </p>
+const Home: React.FC<HomeProps> = ({ message }) => {
+  const router = useRouter();
 
-      <div style={{ position: "relative", width: "100%", height: "500px" }}>
-        <Image
-          src="/images/rolf-webapp.png"
-          alt="ROLF WebApp"
-          fill
-          style={{ objectFit: "contain" }}
-        />
-      </div>
-    </main>
-  </>
-);
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>
+          {message} v{APP_VERSION}
+        </h1>
+        <p>
+          This page was statically generated at build time using Next.js Static
+          Site Generation (SSG).
+        </p>
+
+        <div className="rid grid-cols-2 gap-4">
+          <button
+            className="px-4 py-2 bg-blue-600 text-white rounded"
+            onClick={() => router.push("/")}
+          >
+            Home (SSG) Page
+          </button>
+
+          <button
+            className="px-4 py-2 bg-green-600 text-white rounded"
+            onClick={() => router.push("/ssr")}
+          >
+            SSR Page
+          </button>
+        </div>
+
+        <div style={{ position: "relative", width: "100%", height: "500px" }}>
+          <Image
+            src="/images/rolf-webapp-ssg.png"
+            alt="ROLF WebApp"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Home;
--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import { APP_VERSION } from "../lib/version";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface SSRProps {
   message: string;
@@ -17,27 +18,47 @@ export const getServerSideProps = async () => {
   };
 };
 
-const SSRPage: React.FC<SSRProps> = ({ message, time }) => (
-  <>
-    <Header />
-    <main>
-      <h1>
-        {message} v{APP_VERSION}
-      </h1>
-      <p>
-        This page was dynamically generated using Next.js Server-Side Rendering
-        (SSR). <b>Rendered on the server at: {time}</b>
-      </p>
-      <div style={{ position: "relative", width: "100%", height: "500px" }}>
-        <Image
-          src="/images/rolf-webapp.svg"
-          alt="ROLF WebApp"
-          fill
-          style={{ objectFit: "contain" }}
-        />
-      </div>
-    </main>
-  </>
-);
+const SSRPage: React.FC<SSRProps> = ({ message, time }) => {
+  const router = useRouter();
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>
+          {message} v{APP_VERSION}
+        </h1>
+        <p>
+          This page was dynamically generated using Next.js Server-Side
+          Rendering (SSR). <b>Rendered on the server at: {time}</b>
+        </p>
+
+        <div className="grid grid-cols-2 gap-4">
+          <button
+            className="px-4 py-2 bg-blue-600 text-white rounded"
+            onClick={() => router.push("/")}
+          >
+            Home (SSG) Page
+          </button>
+
+          <button
+            className="px-4 py-2 bg-green-600 text-white rounded"
+            onClick={() => router.push("/ssr")}
+          >
+            SSR Page
+          </button>
+        </div>
+
+        <div style={{ position: "relative", width: "100%", height: "500px" }}>
+          <Image
+            src="/images/rolf-webapp-ssr.png"
+            alt="ROLF WebApp"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default SSRPage;
